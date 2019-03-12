50a580afa1d9c3707b281f2188b332193356d74c
hedayatiR <gunner_1369@yahoo.com>
2019-03-12 22:30:06 +0330

    Adds 2 constructors for BloodData

--- BloodData.java
+++ BloodData.java
@@ -2,6 +2,16 @@ public class BloodData {
     private String bloodType;
     private String RH;
 	//--------------------------------------------
+	    public BloodData() {
+        this.bloodType = "O";
+        this.RH = "+";
+    }
+	//--------------------------------------------
+    public BloodData(String bloodType, String RH) {
+        this.bloodType = bloodType;
+        this.RH = RH;
+    }
+	//--------------------------------------------
     public void setBloodType(String bloodType) {
         this.bloodType = bloodType;
     }
