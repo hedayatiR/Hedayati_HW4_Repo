2957ff1502a9f1a33df51cc5544ce7cbe9d08105
hedayatiR <gunner_1369@yahoo.com>
2019-03-12 22:28:08 +0330

    Adds BloodData class with setters and getters of bloodType & RH

---
+++ BloodData.java
@@ -0,0 +1,21 @@
+public class BloodData {
+    private String bloodType;
+    private String RH;
+	//--------------------------------------------
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+	//--------------------------------------------
+    public void setRH(String RH) {
+        this.RH = RH;
+    }
+	//--------------------------------------------
+    public String getBloodType() {
+        return bloodType;
+    }
+	//--------------------------------------------
+    public String getRH() {
+        return RH;
+    }
+	//--------------------------------------------
+}
