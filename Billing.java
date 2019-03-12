297d72462a4d2d573e339e21eeb26d442503c13b
hedayatiR <gunner_1369@yahoo.com>
2019-03-12 22:20:03 +0330

    Adds main method for test

--- Billing.java
+++ Billing.java
@@ -11,4 +11,23 @@ public class Billing {
         return computeBill(price, count) - 1.08*coupon;
     }
     //------------------------------------------------
+    public static void main(String[] args) {
+        Billing bill = new Billing();
+        double price = 100;
+        int count = 5;
+        int coupon = 50;
+
+        // 1 input
+        System.out.println("computeBill with price:");
+        System.out.println(bill.computeBill(price));
+        System.out.println();
+        // 2 input
+        System.out.println("computeBill with price & count:");
+        System.out.println(bill.computeBill(price, count));
+        System.out.println();
+        // 3 input
+        System.out.println("computeBill with price & count & coupon:");
+        System.out.println(bill.computeBill(price, count, coupon));
+    }
+	//------------------------------------------------
 }
